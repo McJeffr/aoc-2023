f36da970a92e1626749f4482bfc77d7b3375f134
McJeffr <mcjeffr@gmail.com>
2023-12-01 17:19:44 +0100

    Day 1 part 1.

--- src/main/kotlin/com/mcjeffr/aoc/days/Day1.kt
+++ src/main/kotlin/com/mcjeffr/aoc/days/Day1.kt
@@ -3,13 +3,18 @@ package com.mcjeffr.aoc.days
 import com.mcjeffr.aoc.common.PuzzleInput
 
 fun main() {
-    val input = PuzzleInput.getExampleInput(1)
+    val input = PuzzleInput.getInput(1)
     part1(input)
     part2(input)
 }
 
 fun part1(input: List<String>) {
-
+    val sum = input.sumOf { line ->
+        val leftDigit = line.first { it.isDigit() }
+        val rightDigit = line.last { it.isDigit() }
+        "$leftDigit$rightDigit".toInt()
+    }
+    println("The sum of the lines of text is '$sum'")
 }
 
 fun part2(input: List<String>) {
